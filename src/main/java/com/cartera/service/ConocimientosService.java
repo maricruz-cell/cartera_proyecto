4a19711f39b2896d485083351f06b7c56835621c
Maricruz <hmaricruz720@gmail.com>
2025-12-03 13:19:48 +1200

    Actualizando 3 dic

--- src/main/java/com/cartera/service/ConocimientosService.java
+++ src/main/java/com/cartera/service/ConocimientosService.java
@@ -1,8 +1,4 @@
 package com.cartera.service;
 
-import com.cartera.dto.ConocimientosDTO;
-
-public interface ConocimientosService {
-    ConocimientosDTO cargarConocimientos(Long idPersona);
-    void guardarConocimientos(ConocimientosDTO dto);
+public class ConocimientosService {
 }
